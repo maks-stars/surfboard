7d3e2682fc23c951e2dd87ff6527a5c5a5333915
Maxim Kostenko <126064613+maks-stars@users.noreply.github.com>
2023-05-14 19:23:39 +0300

    Add files via upload

--- script/map.js
+++ script/map.js
@@ -12,10 +12,11 @@ const init = () => {
    myCollection = new ymaps.GeoObjectCollection({}, {
      draggable: false,
      iconLayout: 'default#image',
-     iconImageHref: '../img/icons/geo.svg',
+     iconImageHref: './../img/icons/geo.svg',
      iconImageSize: [58, 74],
      iconImageOffset: [-35, -52]
    });
+   
  
  for (let i = 0; i < coords.length; i++) {
    myCollection.add(new ymaps.Placemark(coords[i]));
